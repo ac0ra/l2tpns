aaa4ac9e7e4e927d505181940e2f20296f8e1168
rmcleay <rmcleay@90ab53b2-951c-0410-afb2-da77b783a476>
2007-07-09 00:48:45 +0000

    Fixed the calling station ID issue and created more logging.

--- editcsid.c
+++ editcsid.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <ctype.h>
 #include "l2tpns.h"
 #include "plugin.h"
 
@@ -14,10 +15,10 @@ int plugin_pre_auth(struct param_pre_auth *data)
 	char *p, *tmp;
 	char tmp_calling[MAXTEL];
 	tmp = &tmp_calling[0];
+	int i;
 
 	if (!data->continue_auth) return PLUGIN_RET_STOP;
 
-	//if (!(data->s->calling[0] == '6' && data->s->calling[1] == '1')) {
 	if (!(data->s->calling[0] == '6' && data->s->calling[1] == '1')) {
 		f->log(3, 0, 0, "Not altering Calling Station ID from \"%s\"\n", data->s->calling);
 		return PLUGIN_RET_OK;
@@ -29,18 +30,26 @@ int plugin_pre_auth(struct param_pre_auth *data)
 	p = &data->s->calling[2];
 	//Copy in the remaining part of the username and null terminate
 	strncpy(tmp + 1, p, strlen(data->s->calling) - 2);
-	tmp[strlen(data->s->calling)] = 0;
+	
+	//tmp[strlen(data->s->calling)] = 0;
+	//We'll always have a 10 digit number by now
+	tmp[10] = 0;
+	
+	//Check to make sure that each digit is actually a digit. If it isn't,
+	//we want to disconnect the number.
+	for (i=0;i<strlen(tmp);i++) {
+		if (0 != isdigit(tmp[i])) {
+			data->auth_allowed = 0;
+			data->continue_auth = 0;
+			f->log(3, 0, 0, "ERROR: Calling station ID incorrect (\"%s\"). Disallowing login.", data->s->calling);
+			return PLUGIN_RET_STOP;
+		}
+	}
+
+	f->log(3, 0, 0, "Altering Calling Station ID from \"%s\" to \"%s\"\n", data->s->calling, tmp);
 
 	//Assign this to both the username and the calling station id
 	strncpy(data->s->calling, tmp, MAXTEL);
-
-	// It's highly unlikely that the following lines will
-	// be required, thanks to MatW's patch.
-
-	// strncpy(data->s->user, tmp, MAXTEL);
-	// strncpy(data->username, tmp, MAXTEL);
-
-	f->log(3, 0, 0, "Altering Calling Station ID to \"%s\"\n", tmp);
 
 	return PLUGIN_RET_OK;
 }
