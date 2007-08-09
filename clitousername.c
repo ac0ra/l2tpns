1abeb478ce793c801cecaf449b667046baf8e9fc
rmcleay <rmcleay@90ab53b2-951c-0410-afb2-da77b783a476>
2007-08-09 00:12:45 +0000

    Fixed the comment

--- clitousername.c
+++ clitousername.c
@@ -2,7 +2,7 @@
 #include "l2tpns.h"
 #include "plugin.h"
 
-/* change 61x into 0x for Australian Phone numbers */
+/* change the username into a copy of the calling station id */
 
 char const *cvs_id = "Id: clitousername.c,v 1.0 2007/05/31 010:50:53 matw Exp $";
 
@@ -11,7 +11,7 @@ static struct pluginfuncs *f = 0;
 
 int plugin_pre_auth(struct param_pre_auth *data)
 {
-		f->log(3, 0, 0, "Setting username as CLI..\n");
+	f->log(3, 0, 0, "Setting username as CLI..\n");
         free(data->username);
         data->username = strdup(data->s->calling);
         f->log(3, 0, 0, "Username is now %s\n",data->username);
