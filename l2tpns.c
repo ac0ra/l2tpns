dec7dbfc5d3e9db36745b6dc7fd020d933366e1f
bkolera <bkolera@90ab53b2-951c-0410-afb2-da77b783a476>
2009-01-15 04:22:25 +0000

    Trying to add a log level to l2tpns to debug the contents of the session in dump_session

--- l2tpns.c
+++ l2tpns.c
@@ -4216,6 +4216,8 @@ void snoop_send_packet(uint8_t *packet, uint16_t size, in_addr_t destination, ui
 
 static int dump_session(FILE **f, sessiont *s)
 {
+	LOG(4, 0, 0, "Dumping session params for dump session: %d %u %u %s %hx \n", s->ip, s->cin_delta, s->cout_delta, s->user, s->walled_garden);
+	
 	if (!s->opened || !s->ip || !(s->cin_delta || s->cout_delta) || !*s->user || s->walled_garden)
 		return 1;
         time_t now = time(NULL);
